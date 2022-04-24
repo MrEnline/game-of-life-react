ecd3957d5295af5ee7769ad9bd5eaf5c9b5b13ec
Kuznetsov Sergey <mr.enline@gmail.com>
2022-04-24 21:38:46 +0300

    add module class for styles

--- src/components/field/Field.js
+++ src/components/field/Field.js
@@ -1,5 +1,5 @@
-import "./Field.scss"
 import classNames from "classnames";
+import styles from "./Field.module.scss"
 import {FIELD_SIZE, BOX_SIZE} from '../../utils/Constants'
 
 const Field = ({runGame, liveBoxMap, onChangeField}) => {
@@ -8,7 +8,7 @@ const Field = ({runGame, liveBoxMap, onChangeField}) => {
         const arrItems = [];
         for (let i = 1; i <= FIELD_SIZE.columns; i++) {
             for(let j = 1; j <= FIELD_SIZE.rows; j++){
-                arrItems.push(<div className={classNames('box', {'box-color': liveBoxMap[`${i}_${j}`]})}
+                arrItems.push(<div className={classNames(styles.box, {[styles.box_color]: liveBoxMap[`${i}_${j}`]})}
                                     key={`${(i - 1) * FIELD_SIZE.columns + j}`}
                                     data-xy={`${i}_${j}`}
                                     onClick={() => changeBackgroundColorBox(`${i}_${j}`)}>
@@ -33,7 +33,7 @@ const Field = ({runGame, liveBoxMap, onChangeField}) => {
     const items = generateField();
     
     return (
-        <div className="field" style={{'width' : FIELD_SIZE.columns * BOX_SIZE}}>
+        <div className={styles.field} style={{'width' : FIELD_SIZE.columns * BOX_SIZE}}>
             {items}
         </div>
     );
